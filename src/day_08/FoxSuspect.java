d6300b95d3320526b166bcd8998dff5fca223c52
Korshad2012 <gihtilali@gmail.com>
2022-06-06 16:39:16 -0400

    Fox Suspect

---
+++ src/day_08/FoxSuspect.java
@@ -0,0 +1,30 @@
+package day_08;
+
+import java.util.Scanner;
+
+public class FoxSuspect {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Does fox wear black coat and carry three bags?: true/false");
+        boolean answer = input.nextBoolean();
+
+        System.out.println(checkSuspect(answer));
+
+
+        input.close();
+
+
+    }
+
+    public static String checkSuspect(boolean answer) {
+        if (answer) {
+            return "Fox is one of the suspects!";
+        } else {
+            return "Fox cleared of charges";
+        }
+    }
+
+}
