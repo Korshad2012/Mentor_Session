b6bdd2b5ea535600426f438ef3b6bfe55899dcca
Korshad2012 <gihtilali@gmail.com>
2022-06-08 12:00:52 -0400

    Fox Suspect

---
+++ src/day_08/CheckFoxSuspect.java
@@ -0,0 +1,14 @@
+package day_08;
+
+public class CheckFoxSuspect {
+
+    public static void main(String[] args) {
+
+        Fox fox1 = new Fox();
+
+        fox1.setInfo("Fox1",true,true,"black",0);
+
+        fox1.isSuspect();
+
+    }
+}
