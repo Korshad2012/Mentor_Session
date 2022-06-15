320a3e448d021c6d9111106d33d5a2263e453e5b
Korshad2012 <gihtilali@gmail.com>
2022-06-15 12:39:51 -0400

    Practice

--- src/day_09/Fighters.java
+++ src/day_09/Fighters.java
@@ -14,24 +14,5 @@ public class Fighters {
     }
 
 
-    public static String result(Fighters fighter1, Fighters fighter2, String firstAttacker) {
 
-        boolean turn1 = fighter1.name == firstAttacker;
-        while (true) {
-            if (turn1) {
-                fighter2.health -= fighter1.damagePerAttack;
-                if (fighter2.health <= 0)
-                    return fighter1.name+ " Wins";
-            } else {
-                fighter1.health -= fighter2.damagePerAttack;
-                if (fighter1.health <= 0)
-                    return fighter2.name + " Wins";
-            }
-            turn1 = !turn1;
-
-
-        }
-
-
-    }
 }
--- src/day_09/TwoFighterOneWinner.java
+++ src/day_09/TwoFighterOneWinner.java
@@ -13,15 +13,46 @@ public class TwoFighterOneWinner {
         fighter2.setInfo("Harry",5,4);
 
 
-         String winner = Fighters.result(fighter1,fighter2,fighter1.name);
+         String winner = result(fighter1,fighter2,fighter1.name);
 
         System.out.println(winner);
 
+        Fighters fighters3 = new Fighters();
+        fighters3.setInfo("Jerry",30,3);
+
+        Fighters fighters4 = new Fighters();
+        fighters3.setInfo("Harold",20,5);
+
+        String winner2 = result(fighters3,fighters4, fighters3.name);
+
+        System.out.println(winner2);
 
 
 
 
 
+
+
+
+    }
+
+    public static String result(Fighters fighter1, Fighters fighter2, String firstAttacker) {
+
+        boolean turn1 = fighter1.name == firstAttacker;
+        while (true) {
+            if (turn1) {
+                fighter2.health -= fighter1.damagePerAttack;
+                if (fighter2.health <= 0)
+                    return fighter1.name+ " Wins";
+            } else {
+                fighter1.health -= fighter2.damagePerAttack;
+                if (fighter1.health <= 0)
+                    return fighter2.name + " Wins";
+            }
+            turn1 = !turn1;
+
+
+        }
 
 
     }
